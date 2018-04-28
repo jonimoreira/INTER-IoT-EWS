b7e14702c301ff9211433b4b3f17f33ff312390d
João Moreira <jonimoreira@gmail.com>
2018-04-28 21:33:18 +0200

    Azure IoTHub - identity
    Solution for creating device identity (key, id) in Azure IoTHub

---
+++ WP2-INTER-IoT-EWS/src/ShimmerAzureIoTHubCreateDeviceIdentity/ShimmerAzureIoTHubCreateDeviceIdentity/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace ShimmerAzureIoTHubCreateDeviceIdentity
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=MyDrivingIoTHubEWS.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=tCI9KK86Dpkd2c22WbsRFzTQX0uMjrxnKzu1bpsM1ZI=";
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "ZY224DC54P";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+
+    }
+}
